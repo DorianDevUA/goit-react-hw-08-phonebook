ce1c1496d41eb6cdd7c1ee59b20c668909dad5b6
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-28 19:16:45 +0400

    add GlobalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'constants/theme';
+
+import 'modern-normalize';
+import GlobalStyle from 'styles/GlobalStyle';
+
+import App from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook">
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
