59d6fae6eec2952e523c4b64a5e3c3e5bfc7a95a
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-28 18:23:47 +0400

    add Routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,23 @@
+import Layout from 'components/Layout';
+import HomePage from 'pages/HomePage';
+import LoginPage from 'pages/LoginPage';
+import PhonebookPage from 'pages/PhonebookPage';
+import RegistrationPage from 'pages/RegistrationPage';
+import { Route, Routes } from 'react-router-dom';
+
 const App = () => {
-  return <div>App Component</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="phonebook" element={<PhonebookPage />} />
+          <Route path="register" element={<RegistrationPage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
