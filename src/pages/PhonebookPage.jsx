0e8cc4de7166fdeef6f2ff06f793bd79d169e033
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-31 18:47:36 +0400

    add components to the page

--- src/pages/PhonebookPage.jsx
+++ src/pages/PhonebookPage.jsx
@@ -1,7 +1,48 @@
+import ContactForm from 'components/ContactForm';
+import ContactList from 'components/ContactList';
+import IconButton from 'components/IconButton';
+import { ImUserPlus } from 'react-icons/im';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { contactsOperations, contactsSelectors } from 'redux/contacts';
+import Modal from 'components/Modal';
+import { modalSelectors } from 'redux/modal';
+import { toggleModal } from 'redux/modal/slice';
+
 const PhonebookPage = () => {
+  const dispatch = useDispatch();
+  const isModalOpen = useSelector(modalSelectors.selectShowModal);
+  const filteredContacts = useSelector(contactsSelectors.selectContacts);
+  console.log('isModalOpen', isModalOpen);
+
+  useEffect(() => {
+    dispatch(contactsOperations.getAll());
+  }, [dispatch]);
+
+  // const filteredContacts = [
+  //   { id: 1, name: 'Alex', number: '890-90-90' },
+  //   { id: 2, name: 'Leyla', number: '530-40-95' },
+  //   { id: 3, name: 'Tymur', number: '120-10-10' },
+  // ];
+
   return (
     <>
-      <div>PhonebookPage</div>
+      <h2>Contacts</h2>
+      <IconButton
+        type="button"
+        title="Add contact"
+        text="Add contact"
+        onClick={() => dispatch(toggleModal())}
+        aria-label="Add new contact"
+      >
+        <ImUserPlus />
+      </IconButton>
+      {isModalOpen && (
+        <Modal>
+          <ContactForm />
+        </Modal>
+      )}
+      <ContactList contacts={filteredContacts} />
     </>
   );
 };
