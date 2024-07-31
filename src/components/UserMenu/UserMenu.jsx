6317013a765cf1b5ba6222c771f6cc1a1e867800
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-31 18:45:47 +0400

    add authOperations.logOut

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,14 +1,25 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import authSelectors from 'redux/auth/selectors';
 import { Container } from './UserMenu.styled';
+import defaultAvatar from './default-avatar.png';
+import { authOperations } from 'redux/auth';
 
 const UserMenu = () => {
+  const dispatch = useDispatch();
   const userName = useSelector(authSelectors.selectUserName);
+  const avatar = defaultAvatar;
+
+  const handleLogOut = () => {
+    dispatch(authOperations.logOut());
+  };
 
   return (
     <Container>
+      <img src={avatar} alt={userName} width="32" />
       <span>Вітаємо, {userName}</span>
-      <button type="button">Вийти</button>
+      <button type="button" onClick={handleLogOut}>
+        Вийти
+      </button>
     </Container>
   );
 };
