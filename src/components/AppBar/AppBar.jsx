d143a1f86d6343f15507c9964d66e5065cbb3afe
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-31 18:38:49 +0400

    add conditional rendering

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,13 +2,16 @@ import AuthNav from 'components/AuthNav';
 import Navigation from 'components/Navigation';
 import UserMenu from 'components/UserMenu';
 import { Container } from './AppBar.styled';
+import { useSelector } from 'react-redux';
+import { authSelectors } from 'redux/auth';
 
 const AppBar = () => {
+  const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
+
   return (
     <Container>
       <Navigation />
-      <AuthNav />
-      <UserMenu />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </Container>
   );
 };
