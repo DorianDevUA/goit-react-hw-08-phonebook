1c89fea89cc1cf68a346f502978f1fafb8163453
Dorian Devua <Dorian.DevUa@gmail.com>
2024-08-01 19:31:18 +0400

    add redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,24 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'constants/theme';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
 
 import 'modern-normalize';
 import GlobalStyle from 'styles/GlobalStyle';
 
 import App from 'components/App';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
-          <GlobalStyle />
-          <App />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <GlobalStyle />
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './reducer';
+// import { rootReducer } from './reducer';
+import { contactsReducer } from './contacts/slice';
+import { authReducer } from './auth/slice';
+import { modalReducer } from './modal/slice';
+import { filterReducer } from './filter';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    contacts: contactsReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
+    modal: modalReducer,
+    filter: filterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
